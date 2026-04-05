7f45d80fee75b3814a7acf6e493c907e01dc7bce
Daudft <alikhaft45@gmail.com>
2026-04-05 15:37:58 +0500

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./app.scss"
 import Slide from './components/slide/Slide'
+import Navbar from './components/navbar/Navbar'
 
 const App = () => {
   return (
     <>
     <main>
+      <Navbar/>
       <Slide/>
     </main>
       
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./navbar.scss"
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <div className="left">
+            <h1>Daud Afzal</h1>
+            <h2>Work</h2>
+        </div>
+        <div className="right"></div>
+    </nav>
+  )
+}
+
+export default Navbar
