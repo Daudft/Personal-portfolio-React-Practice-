ab63b8be8c5a5aa90e6b56cdfc89ce031e6b3afe
Daudft <alikhaft45@gmail.com>
2026-04-06 18:25:17 +0500

    hero updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "./app.scss"
 import Slide from './components/slide/Slide'
-import Navbar from './components/navbar/Navbar'
+
+import Hero from './components/hero-txt/Hero'
 
 const App = () => {
   return (
     <>
     <main>
-      <Navbar/>
+      
+      <Hero/>
       <Slide/>
     </main>
       
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./hero.scss"
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <div className='hero__marquee'>
+        <div className='hero__track hero__track--ltr'>
+          <h1>Designer and developer crafting bold digital experiences</h1>
+          <h1>Designer and developer crafting bold digital experiences</h1>
+        </div>
+
+        <div className='hero__track hero__track--rtl'>
+          <h1>Designer and developer crafting bold digital experiences</h1>
+          <h1>Designer and developer crafting bold digital experiences</h1>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
